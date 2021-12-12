ebbb07d30fbe55b3e58dfb892365999ae57bd6b4
Patrick Carlos <patrickmcarlos@hotmail.com>
2021-12-12 01:17:21 -0800

    adds day2 pt1 and pt2 solutions

---
+++ src/day2/day2_pt1.ts
@@ -0,0 +1,26 @@
+import input from './data';
+
+const getProductOfHorizontalAndDepth = (arr: string[]) => {
+    let depth = 0;
+    let horizontal = 0;
+
+    for (let instruction of arr) {
+        const [action, num] = instruction.split(' ');
+
+        switch (action) {
+            case 'forward':
+                horizontal += Number(num);
+                break;
+            case 'down':
+                depth += Number(num);
+                break;
+            case 'up':
+                depth -= Number(num);
+                break;
+        }
+    }
+
+    return depth * horizontal;
+}
+
+console.log(getProductOfHorizontalAndDepth(input));
---
+++ src/day2/day2_pt2.ts
@@ -0,0 +1,29 @@
+import input from './data';
+
+const getProductOfHorizontalAndDepth = (arr: string[]) => {
+    let depth = 0;
+    let horizontal = 0;
+    let aim = 0;
+
+    for (let instruction of arr) {
+        const [action, numStr] = instruction.split(' ');
+        const num = Number(numStr);
+
+        switch (action) {
+            case 'forward':
+                horizontal += num;
+                depth += aim * num;
+                break;
+            case 'down':
+                aim += num;
+                break;
+            case 'up':
+                aim -= num;
+                break;
+        }
+    }
+
+    return depth * horizontal;
+}
+
+console.log(getProductOfHorizontalAndDepth(input));
