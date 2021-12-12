6556af5ec6ac4986e129059c9bc2fea11bcce589
Patrick Carlos <patrickmcarlos@hotmail.com>
2021-12-12 01:30:12 -0800

    adds day3 pt1 soln

---
+++ src/day3/day3_pt1.ts
@@ -0,0 +1,36 @@
+import input from './data';
+
+const getPowerConsumption = (input: string[]) => {
+    let gamma = "";
+    let epsilon = "";
+    let numDigits = input[0].length;
+
+    for (let i = 0; i < numDigits; i++) {
+        let ones = 0;
+        let zeros = 0;
+
+        for (const numStr of input) {
+            const val = numStr.charAt(i);
+
+            if (val === "1") {
+                ones++;
+            } else {
+                zeros++;
+            }
+        }
+
+        if (ones > zeros) {
+            gamma += "1";
+            epsilon += "0";
+        } else {
+            gamma += "0";
+            epsilon += "1";
+        }
+    }
+
+    const sum = parseInt(gamma, 2) * parseInt(epsilon, 2);
+
+    return sum;
+}
+
+console.log(getPowerConsumption(input));
