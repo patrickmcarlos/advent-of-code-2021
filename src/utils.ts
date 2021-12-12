b914a259582ee3754349d3187632d5096f3e2c46
Patrick Carlos <patrickmcarlos@hotmail.com>
2021-12-12 13:44:18 -0800

    adds utils fx for verifying board, updating board

--- src/utils.ts
+++ src/utils.ts
@@ -22,3 +22,72 @@ export const strTo3dMatrixHelper = (str: string): number[][][] => {
 
     return res;
 };
+
+// Marked numbers are denoted by -1 on the board.
+export const verifyBingo = (board: number[][]): boolean => {
+    // The board will typically be a square.
+    const height = board.length;
+    const width = board[0].length;
+
+    // Find horizontal wins (rows)
+    for (let x = 0; x < height; x++) {
+        const row = board[x];
+        const isWinner = !row.some((val) => val !== -1);
+
+        if (isWinner) {
+            return true;
+        }
+    }
+
+    // Find vertical wins (columns)
+    for (let y = 0; y < width; y++) {
+        let isWinner = true;
+
+        for (let x = 0; x < height; x++) {
+            const val = board[x][y];
+
+            if (val !== -1) {
+                isWinner = false;
+                break;
+            }
+        }
+
+        if (isWinner) {
+            return true;
+        }
+    }
+
+    let isWinner = true;
+    // Find diagonals (top left to bottom right)
+    for (let x = 0, y = 0; x < height && y < width; x++, y++) {
+        if (board[x][y] !== -1) {
+            isWinner = false;
+            break;
+        }
+    }
+
+    if (isWinner) {
+        return true;
+    }
+
+    isWinner = true;
+    // Find diagonals (bottom left to top right)
+    for (let x = height - 1, y = 0; x >= 0 && y < width; x--, y++) {
+        if (board[x][y] !== -1) {
+            isWinner = false;
+            break;
+        }
+    }
+
+    return isWinner;
+};
+
+export const updateBoard = (board: number[][], numberCalled: number): void => {
+    for (let row of board) {
+        for (let i = 0; i < row.length; i++) {
+            if (row[i] === numberCalled) {
+                row[i] = -1;
+            }
+        }
+    }
+};
